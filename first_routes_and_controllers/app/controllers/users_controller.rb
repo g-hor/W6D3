f581ed8df48ce268b9c37816c6c8d25e5938b405
Shan Kim <shankim35@gmail.com>
2023-02-15 15:33:39 -0500

    Phase 2

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
 
+    def index
+        # @users = User.all
+        # render json: @users
+        render plain: "I'm in the index action!"
+    end
 
+    # def show
+    #     render json: User.find(params[:id])
+    # end
+
+    # def create
+    #     @user = User.new(user_params)
+
+
+    # end
 
 end
