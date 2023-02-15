a2e3c774d977601f8c940b9f4064bc8e0b857b96
Gary <geehor@gmail.com>
2023-02-15 15:15:48 -0500

    Finish Phase 1

---
+++ first_routes_and_controllers/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  # get '/users/new', to: 'users#new', as: 'new_user'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
