f3fae31cc0f6b35c86ecdb43a1d85a40425b974b
Shan Kim <shankim35@gmail.com>
2023-02-15 16:42:00 -0500

    phase 4

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,20 +1,36 @@
 class UsersController < ApplicationController
 
+
     def index
-        # @users = User.all
-        # render json: @users
-        render json: params
+        @users = User.all
+        render json: @users
+        # render json: params
     end
 
     def show
-        # render json: User.find(params[:id])
-        render json: params
+        render json: User.find(params[:id])
+        # render json: params
     end
 
     def create
-        # @user = User.new(user_params)
-        render json: params
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
 
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user.id)
+            
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 
 end
---
+++ first_routes_and_controllers/db/migrate/20230215212141_change_users.rb
@@ -0,0 +1,6 @@
+class ChangeUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :email, :string, null: false
+    
+  end
+end
