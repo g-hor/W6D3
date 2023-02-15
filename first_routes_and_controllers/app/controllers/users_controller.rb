b58b61d8bcedfabe33767ac98811766ad3444380
Gary <geehor@gmail.com>
2023-02-15 16:20:12 -0500

    Finish Phase 3

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -3,17 +3,18 @@ class UsersController < ApplicationController
     def index
         # @users = User.all
         # render json: @users
-        render plain: "I'm in the index action!"
+        render json: params
     end
 
-    # def show
-    #     render json: User.find(params[:id])
-    # end
+    def show
+        # render json: User.find(params[:id])
+        render json: params
+    end
 
-    # def create
-    #     @user = User.new(user_params)
+    def create
+        # @user = User.new(user_params)
+        render json: params
 
-
-    # end
+    end
 
 end
