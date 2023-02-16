8d6b30241618a35351f0a9865eccf310b7074f7d
Gary <geehor@gmail.com>
2023-02-16 10:59:34 -0500

    Finish Phase 1: Artwork

--- first_routes_and_controllers/app/models/artwork.rb
+++ first_routes_and_controllers/app/models/artwork.rb
@@ -1,3 +1,6 @@
 class Artwork < ApplicationRecord
-  validates :title, uniqueness: { scope: :artist_id }
+  validates :title, presence: true, uniqueness: { scope: :artist_id }
+  validates :image_url, uniqueness: true, presence: true
+  validates :artist_id, presence: true
+  
 end
