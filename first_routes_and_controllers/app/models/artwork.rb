0e7fdd7115f60f9069a9fc8cc5b9f18e05ad958d
Shan Kim <shankim35@gmail.com>
2023-02-16 13:35:37 -0500

    Seeding Database

--- first_routes_and_controllers/app/models/artwork.rb
+++ first_routes_and_controllers/app/models/artwork.rb
@@ -10,6 +10,9 @@
 #  updated_at :datetime         not null
 #
 class Artwork < ApplicationRecord
+
+  attr_reader :id
+
   validates :title, presence: true, uniqueness: { scope: :artist_id }
   validates :image_url, uniqueness: true, presence: true
   validates :artist_id, presence: true
--- first_routes_and_controllers/db/seeds.rb
+++ first_routes_and_controllers/db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do
+
+  User.destroy_all
+  Artwork.destroy_all
+  ArtworkShare.destroy_all
+
+  %w(users artworks artwork_shares).each do |table_name|
+    ApplicationRecord.connection.reset_pk_sequence!(table_name)
+  end
+
+
+  #users
+  u1 = User.create!(username: "Shan")
+  u2 = User.create!(username: "Gary")
+  u3 = User.create!(username: "Alvin")
+  u4 = User.create!(username: "Amin")
+
+
+  #artworks
+  a1 = Artwork.create!(title: "whatever", image_url: "1", artist_id: 1)
+  a2 = Artwork.create!(title: "alsowhatever", image_url: "2", artist_id: 1)
+  a3 = Artwork.create!(title: "monalisa", image_url: "3", artist_id: 3)
+  a4 = Artwork.create!(title: "inches", image_url: "4", artist_id: 4)
+
+
+  #artworkshares
+  as1 = ArtworkShare.create!(artwork: a1, viewer: u1)
+  as2 = ArtworkShare.create!(artwork: a1, viewer: u2)
+  as3 = ArtworkShare.create!(artwork: a2, viewer: u3)
+  as4 = ArtworkShare.create!(artwork: a3, viewer: u1)
+  as5 = ArtworkShare.create!(artwork: a4, viewer: u1)
+
+
+
+end
